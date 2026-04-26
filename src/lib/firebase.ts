72ffcd9f4af14ac352be1fc2082e67604c664b89
mattewinflames <matteo.pastore.92@gmail.com>
2026-04-26 19:12:08 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ import {
   User 
 } from 'firebase/auth';
 import { 
-  getFirestore, 
+  initializeFirestore, 
   collection, 
   doc, 
   addDoc, 
@@ -25,25 +25,14 @@ import {
   getDoc,
   getDocFromServer,
   serverTimestamp,
-  enableIndexedDbPersistence
+  persistentLocalCache
 } from 'firebase/firestore';
 import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-
-// Enable offline persistence
-if (typeof window !== 'undefined') {
-  enableIndexedDbPersistence(db).catch((err) => {
-    if (err.code === 'failed-precondition') {
-      // Multiple tabs open, persistence can only be enabled in one tab at a a time.
-      console.warn('Firestore persistence failed-precondition');
-    } else if (err.code === 'unimplemented') {
-      // The current browser does not support all of the features required to enable persistence
-      console.warn('Firestore persistence unimplemented');
-    }
-  });
-}
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache()
+});
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
