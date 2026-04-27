d40e10cd2653061454d58726d63b78a414e179dc
mattewinflames <matteo.pastore.92@gmail.com>
2026-04-27 11:25:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
+     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
